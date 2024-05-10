2fd4d23b1968308f513a984cc41b33cfeaa426b2
Jesse-L2 <jesse.little@utexas.edu>
2024-05-10 15:25:15 -0500

    Add template classes for some responsive behavior and project layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+// export default function App() {
+//   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+// }
+
+const App = () => (
+  <main className="relative">
+    NAV
+    <section className="xl:padding-1-wide wide:padding-r padding-b">
+      Hero -ish section
+    </section>
+    <section className="padding">Projects</section>
+    <section className="padding">Other Section</section>
+    <section className="padding">Check it out</section>
+    <section className="padding">Footer</section>
+  </main>
+);
+
+export default App;
