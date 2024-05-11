fd610d6c31408350c41c7eb5459945cfe7852b77
Jesse-L2 <jesse.little@utexas.edu>
2024-05-11 12:55:56 -0500

    assets import statement

---
+++ src/assets/icons/index.js
@@ -0,0 +1,39 @@
+import arrow_right from "./arrow-right.svg";
+import chevron_left from "./chevron-left.svg";
+import chevron_right from "./chevron-right.svg";
+import css_icon from "./css_icon.png";
+import email from "./email.png";
+import facebook from "./facebook.svg";
+import git from "./git.png";
+import hamburger from "./hamburger.svg";
+import html from "./html.png";
+import instagram from "./instagram.svg";
+import javascript from "./javascript.png";
+import node from "./node.svg";
+import python from "./python.svg";
+import react from "./react.png";
+import sass from "./sass.png";
+import skype from "./skype.png";
+import twitter from "./twitter.svg";
+import typescript from "./typescript.svg";
+
+export {
+  arrow_right,
+  chevron_left,
+  chevron_right,
+  css_icon,
+  email,
+  facebook,
+  git,
+  hamburger,
+  html,
+  instagram,
+  javascript,
+  node,
+  python,
+  react,
+  sass,
+  skype,
+  twitter,
+  typescript,
+};
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 // TODO: Create and add a header logo and place in src/assets/images
-// import { headerLogo } from "../assets/images";
+import { hd_Logo } from "../assets/logo";
 // import { hamburgerIcon } from '../assets/icons';
 
 const Nav = () => {
@@ -7,10 +7,10 @@ const Nav = () => {
     <header className="absolute w-full padding-x py-8 z-10 ">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          {/* <img src={headerLogo} alt="Logo" width={150} height={33} /> */}
+          <img src={hd_Logo} alt="Logo" width={150} height={33} />
         </a>
         <ul className="flex flex-1 justify-center items-center gap-12 max-lg:hidden">
-          <li></li>
+          <li>test</li>
         </ul>
       </nav>
     </header>
