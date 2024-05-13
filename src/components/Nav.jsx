5fbf4c36ee94ef4415d4efb60ea1d71236c45782
Jesse-L2 <jesse.little@utexas.edu>
2024-05-13 13:50:32 -0500

    Added dynamic navbar with values stored in constants/index.js

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
-// TODO: Create and add a header logo and place in src/assets/images
 import { hd_Logo } from "../assets/logo";
 import { hamburgerIcon } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -10,12 +10,14 @@ const Nav = () => {
           <img src={hd_Logo} alt="Logo" width={150} height={33} />
         </a>
         <ul className="flex flex-1 justify-center items-center gap-12 max-lg:hidden">
-          <li>test</li>
-          <li>test 2</li>
-          <li>test 3</li>
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
         </ul>
         <div className="hidden max-lg:block">
-          <img src={hamburgerIcon} alt="Hamburger" width={44} height={44} />
+          <img src={hamburgerIcon} alt="Menu" width={44} height={44} />
         </div>
       </nav>
     </header>
---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#bio", label: "About Me" },
+  { href: "#my-projects", label: "Projects" },
+  { href: "#contact-me", label: "Contact Me" },
+];
