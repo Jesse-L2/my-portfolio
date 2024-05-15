1dca247e11af02ec0d5ec927c22ecd50d00ba58b
Jesse-L2 <jesse.little@utexas.edu>
2024-05-15 11:09:07 -0500

    Fixed classes in Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -5,20 +5,20 @@ const Projects = () => {
         <p>Projects</p>
       </div>
       <div>
-        <div class="grid-cols-1 sm:grid md:grid-cols-3 ">
-          <div class="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
+        <div className="grid-cols-1 sm:grid md:grid-cols-3 ">
+          <div className="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
             <a href="#!">
               <img
-                class="rounded-t-lg"
+                className="rounded-t-lg"
                 src="https://tecdn.b-cdn.net/img/new/standard/city/041.webp"
                 alt=""
               />
             </a>
-            <div class="p-6">
-              <h5 class="mb-2 text-xl font-medium leading-tight text-black">
+            <div className="p-6">
+              <h5 className="mb-2 text-xl font-medium leading-tight text-black">
                 Card title
               </h5>
-              <p class="mb-4 text-base text-black">
+              <p className="mb-4 text-base text-black">
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
