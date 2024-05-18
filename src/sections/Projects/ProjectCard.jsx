d21eb5b6fcae7c92e8c5d939bdafeb6627cba9ae
Jesse <jesse.little@utexas.edu>
2024-05-18 14:00:23 -0500

    Applied classes to make ProjectCard(s) viewable

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 
 const ProjectCard = ({ id, title, description, img, tags, link, github }) => {
   return (
-    <div>
+    <>
       <div className="grid-cols-1 sm:grid md:grid-cols-3 ">
-        <div className="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0 hover:shadow-xl">
+        <div className="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0 hover:shadow-xl min-w-40">
           <a href={id}>
             <img className="rounded-t-lg" src={img} alt={title} />
           </a>
@@ -29,7 +29,7 @@ const ProjectCard = ({ id, title, description, img, tags, link, github }) => {
           </a>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
