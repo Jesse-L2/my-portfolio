8c07bb6c555d509caeee8211085b3371cc0e8cc6
Jesse-L2 <jesse.little@utexas.edu>
2024-05-20 12:20:17 -0500

    Add mySkills.js

---
+++ src/constants/mySkills.js
@@ -0,0 +1,17 @@
+const myLangs = ["Javascript", "Python"];
+const myFrontEnd = ["Bootstrap", "CSS", "SCSS", "TailwindCSS"];
+const myBackEnd = ["expressJS", "nodeJS", "Django", "Flask"];
+const myCloud = ["AWS", "Heroku", "Netlify", "Hostinger"];
+const myDBs = ["Mongo DB", "SQLite", "PostgreSQL"];
+const myTools = [
+  "Nginx",
+  "Postman",
+  "Git",
+  "GitHub",
+  "VSCode",
+  "PyCharm",
+  "WebStorm",
+  "Jupyter Notebook",
+];
+
+export { myLangs, myFrontEnd, myBackEnd, myCloud, myDBs, myTools };
