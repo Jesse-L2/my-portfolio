f510e8812223f9594c4426c68f3c38eff86ba187
Jesse-L2 <jesse.little@utexas.edu>
2024-05-20 16:45:47 -0500

    Add programatic Skills

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,9 +1,21 @@
+import { myLangs } from "../constants/mySkills";
+
 const Skills = () => {
+  if (!myLangs.length) return null;
+
   return (
-    <section>
-      <div>
-        <p>Skills</p>
-      </div>
+    <section className="section skills" id="skills">
+      <h2 className="text-center uppercase mb-4">Skills</h2>
+      <ul className="max-w-[450px] w-[95%] flex flex-wrap justify-center mx-auto my-0">
+        {myLangs.map((language) => (
+          <li
+            key={language}
+            className="block cursor-pointer font-medium lowercase transition-transform duration-[0.2s] ease-[ease-in-out] px-4 py-3 m-2 text-transform: initial btn btn--plain"
+          >
+            {language}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
