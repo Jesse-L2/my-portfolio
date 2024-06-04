bce0b2b4e282bd5d52627da50e2cba18827bf719
Jesse <jesse.little@utexas.edu>
2024-06-03 19:01:01 -0500

    More hero styles

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,18 +1,16 @@
 const About = () => {
   return (
     <section className="">
-      <div className="flex items-center justify-evenly pr-4 sm:pr-8 ">
-        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold font-montserrat">
+      <div className="flex items-center justify-evenly pt-0 pr-4 sm:pr-8 flex-col md:flex-row ">
+        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold font-montserrat pb-8 md:pb-0">
           <span className="text-light-text">
             Hello, <br></br>I'm Jesse
           </span>
         </h1>
         <img
           src=".\src\assets\images\profile.jpg"
-          alt="Picture of me"
-          height="288"
-          width="288"
-          className="rounded-full object-cover h-24 w-24 sm:h-36 sm:w-36 md:h-48 md:w-48"
+          alt="My Profile Picture"
+          className="rounded-full h-48 w-48  md:h-72 md:w-72"
         />
       </div>
     </section>
--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -2,8 +2,8 @@ import { UT_Logo } from "../assets/logo";
 
 const Education = () => {
   return (
-    <section className="mt-4">
-      <div>
+    <section className="mt-4 padding">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-12 text-center">
         <h1 className="text-3xl font-semibold text-white">Education</h1>
         <img
           className=""
