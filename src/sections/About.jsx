eed7c75b433351e1ae3d07426e9c56098b01e60c
Jesse-L2 <jesse.little@utexas.edu>
2024-06-04 14:31:46 -0500

    Finish About (for now)

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,17 +1,25 @@
 const About = () => {
   return (
     <section className="padding dark:bg-dark">
-      <div className="flex items-center justify-evenly pt-0 pr-4 sm:pr-8 flex-col md:flex-row ">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-montserrat pb-0">
-          <span className="text-light-text">
-            Hello, <br></br>I'm Jesse
-          </span>
-        </h1>
-        <img
-          src=".\src\assets\images\profile_s.jpg"
-          alt="My Profile Picture"
-          className="rounded-full square mx-auto p-12 max-w-96 max-h-96"
-        />
+      <div className="flex sm:gap-10 gap-4 flex-col sm:flex-row items-center sm:justify-evenly">
+        <div className="mb-6 sm:mb-0 w-full sm:w-1/3 text-center">
+          <h1 className="text-6xl font-bold font-montserrat ">
+            <span className="text-blueButterfly-500 ">Jesse</span>
+            <br></br>
+            <span className="text-blueButterfly-500">Little</span>
+            <br></br>
+          </h1>
+          <h3 className="text-large font-semibold dark:text-light-text">
+            Fullstack Developer and Software Engineer
+          </h3>
+        </div>
+        <div className="sm:w-3/5 w-4/5 max-w-96">
+          <img
+            src=".\src\assets\images\profile_s.jpg"
+            alt="My Profile Picture"
+            className="rounded-full "
+          />
+        </div>
       </div>
     </section>
   );
--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -4,7 +4,7 @@ const Education = () => {
   return (
     <section className="mt-0 padding dark:bg-dark ">
       <div className="mx-auto max-w-screen-xl px-4 text-center justify-center items-center">
-        <h1 className="text-3xl font-semibold text-white  mb-6">Education</h1>
+        <h1 className="text-3xl font-bold dark:text-white  mb-6">Education</h1>
         <img
           className="items-center justify-center mx-auto"
           src={UT_Logo}
