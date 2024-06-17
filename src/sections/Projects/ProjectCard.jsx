c8d5dc1790787511f378c395c46da9c77d8c0fb6
Jesse-L2 <jesse.little@utexas.edu>
2024-06-16 19:35:19 -0500

    Adjust project card styles

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({ id, title, description, img, tags, link, github }) => {
     <div className="mx-auto max-w-lg text-center rounded-2xl hover:scale-105 card bg-neutral-900 ">
       <div className="content">
         <a
-          className="block rounded-xl p-8 transition  
+          className="block rounded-xl p-7 transition  
         "
           href={id}
         >
@@ -19,16 +19,16 @@ const ProjectCard = ({ id, title, description, img, tags, link, github }) => {
             />
           </a>
           <div className="px-2 pb-2 rounded-xl bg-neutral-800">
-            <h2 className="my-3 py-2 text-xl font-bold text-white font-montserrat">
+            <h2 className="my-2 py-2 text-xl font-bold text-white font-montserrat">
               {title}
             </h2>
-            <p className="mt-1 text-sm text-neutral-100 font-palanquin">
+            <p className="mt-0 my-1 text-sm text-neutral-100 font-palanquin">
               {description}
             </p>
             <div className="tags mt-4 flex flex-wrap gap-1 justify-center ">
               {tags.map((tag) => (
                 <span
-                  className="whitespace-nowrap rounded-full bg-white hover:bg-purple-200 px-2.5 py-0.5 text-xs text-purple-800 hover:-translate-y-0.5"
+                  className="whitespace-nowrap rounded-full bg-white hover:bg-purple-200 px-2.5 py-[3px] text-xs text-purple-800 hover:-translate-y-0.5"
                   key={tag}
                 >
                   {tag}
@@ -39,7 +39,7 @@ const ProjectCard = ({ id, title, description, img, tags, link, github }) => {
               <a
                 href={github}
                 aria-label="source code"
-                className=" mx-2  hover:rounded-md transition shadow-lg  hover:scale-110"
+                className="mx-2  hover:rounded-md transition shadow-lg  hover:scale-110"
               >
                 <GitHubIcon className="text-white hover:text-green-400" />
               </a>
